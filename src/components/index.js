c2b4ea08bb5b991b521e4902a35a406c4bf5ecfd
cocoa2135 <cocoa2135@gmail.com>
2019-08-12 21:41:26 +0800

    add vue components package demo

---
+++ src/components/index.js
@@ -0,0 +1,21 @@
+// 导入所有组件
+import HelloWorld from './helloworld/index.js'
+
+const components = [HelloWorld]
+const install = function(vue) {
+  /* istanbul ignore if */
+  if (install.installed) return;
+  /*eslint-disable*/
+  components.map((component) => {
+    vue.component(component.name, component); //component.name 此处使用到组件vue文件中的 name 属性
+  });
+};
+/* istanbul ignore if */
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue);
+};
+
+export default {
+  install,
+  HelloWorld
+}
